4fef26999add23e93edac8ff2cca64fba7641a5f
Dennis Dubbert <doener@p058174.nat-gm.FH-Koeln.DE>
2023-05-22 16:40:11 +0200

    removed unused files

--- files/src/types/SessionUpdate.ts
+++
@@ -1,5 +0,0 @@
-import { Prisma } from '@prisma/client'
-
-export type SessionUniqueUpdate = { where: Prisma.SessionWhereUniqueInput, data: Prisma.SessionUpdateInput }
-
-export type SessionUpdate = { where: Prisma.SessionWhereInput, data: Prisma.SessionUpdateInput }
--- files/src/types/UserRole.ts
+++
@@ -1,3 +0,0 @@
-export type UserRole = 'owners' | 'participants'
-
-export const getOtherRole = (role: UserRole) => role === 'owners' ? 'participants' : 'owners'
